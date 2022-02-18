60e6f8ab96577d0070e2dd7af9b0c55281195a49
MCN <84409804+MCN37@users.noreply.github.com>
2022-02-17 21:20:00 -0600

    Update App.js

--- App.js
+++ App.js
@@ -1,28 +1,103 @@
-import 'react-native-gesture-handler';
-import * as React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+¡HOLA GARIEL!
+  No se cargo la carpeta con el archivo HOME, pero borre lo de este archivo y te paso la sintaxis completa va...
 
-import HomeScreen from "./screens/Home";
-import StartMapScreen from "./screens/StarMap";
-import DailyPicScreen from "./screens/DailyPic";
-import SpaceCraftScreen from "./screens/SpaceCraft";
+import React, { Component } from "react";
+import {
+    View,
+    Text,
+    StyleSheet,
+    SafeAreaView,
+    TouchableOpacity,
+    Platform,
+    StatusBar,
+    ImageBackground,
+    Image
+} from "react-native";
 
-const Stack = createStackNavigator();
+export default class HomeScreen extends Component {
 
-function App() {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Principal" screenOptions={{
-        headerShown: false
-      }}>
-        <Stack.Screen name="Principal" component={HomeScreen} />
-        <Stack.Screen name="MapaEstelar" component={StartMapScreen} />
-        <Stack.Screen name="FotoDiaria" component={DailyPicScreen} />
-        <Stack.Screen name="NaveEspacial" component={SpaceCraftScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
+
+    render() {
+        return (
+            <View style={styles.container}>
+                <SafeAreaView style={styles.droidSafeArea} />
+                <ImageBackground source={require('../assets/stars.gif')} style={styles.backgroundImage}>
+                    <View style={styles.titleBar}>
+                        <Image source={require("../assets/main-icon.png")} style={{ width: 150, height: 150 }}></Image>
+                        <Text style={styles.titleText}>Stellar</Text>
+                        <Text style={styles.titleText}>App</Text>
+                    </View>
+
+                    <TouchableOpacity style={styles.routeCard} onPress={() =>
+                        this.props.navigation.navigate("SpaceCraft")
+                    }>
+                        <Text style={styles.routeText}>Spacecrafts</Text>
+                        <Image source={require("../assets/space_crafts.png")} style={styles.routeImage}></Image>
+                    </TouchableOpacity>
+
+                    <TouchableOpacity style={styles.routeCard} onPress={() =>
+                        this.props.navigation.navigate("StarMap")
+                    }>
+                        <Text style={styles.routeText}>Star Map</Text>
+                        <Image source={require("../assets/star_map.png")} style={styles.routeImage}></Image>
+                    </TouchableOpacity>
+
+                    <TouchableOpacity style={styles.routeCard} onPress={() =>
+                        this.props.navigation.navigate("DailyPic")
+                    }>
+                        <Text style={styles.routeText}>Daily Pictures</Text>
+                        <Image source={require("../assets/daily_pictures.png")} style={styles.routeImage}></Image>
+                    </TouchableOpacity>
+                </ImageBackground>
+            </View>
+        );
+    }
 }
 
-export default App;
+const styles = StyleSheet.create({
+    container: {
+        flex: 1
+    },
+    droidSafeArea: {
+        marginTop: Platform.OS === "android" ? StatusBar.currentHeight : 0
+    },
+    backgroundImage: {
+        flex: 1,
+        resizeMode: 'cover',
+    },
+    routeCard: {
+        flex: 0.12,
+        justifyContent: "center",
+        alignItems: "center",
+        margin: 10,
+        marginLeft: 30,
+        marginRight: 30,
+        borderRadius: 100,
+        backgroundColor: "white"
+    },
+    titleBar: {
+        flex: 0.5,
+        justifyContent: "center",
+        alignItems: "center"
+    },
+    titleText: {
+        fontSize: 40,
+        fontWeight: "bold",
+        color: "white"
+    },
+    routeText: {
+        fontSize: 25,
+        fontWeight: "bold",
+        color: '#D11583',
+        justifyContent: "center",
+        alignItems: "center"
+    },
+    routeImage: {
+        position: "absolute",
+        top: -20,
+        right: -15,
+        height: 80,
+        width: 80,
+        resizeMode: "contain"
+    }
+});
